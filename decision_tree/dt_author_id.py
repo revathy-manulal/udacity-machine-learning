0c3c683de5492627963fbeacfa141564e5e2d7e6
Revathy Manulal <revathymanulal@hotmail.com>
2017-12-24 16:27:16 +0530

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
-    Sara has label 0
-    Chris has label 1
-"""
     
 import sys
 from time import time
