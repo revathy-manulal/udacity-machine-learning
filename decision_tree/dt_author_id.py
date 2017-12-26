0c95ac763f8a66565f01c5488b205ff8485c5313
revathy-manulal <revathymanulal@yahoo.com>
2017-12-26 18:05:28 +0530

    Running decision tree with 40 min_samples_split

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,9 +12,20 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+# no of features used
+# print len(features_train[0])
+
+clf1 = tree.DecisionTreeClassifier(min_samples_split=40)
+clf1 = clf1.fit(features_train, labels_train)
+labels_pred = clf1.predict(features_test)
+
+acc_min_samples_split_2 = accuracy_score(labels_pred, labels_test)
