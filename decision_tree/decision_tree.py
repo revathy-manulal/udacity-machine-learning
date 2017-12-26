c86c9cebd4d61fbca6bde7c397fce61014598489
revathy-manulal <revathymanulal@yahoo.com>
2017-12-26 17:07:14 +0530

    More of Decision Tree

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,13 +11,14 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+labels_pred = clf.predict(features_test)
 
+acc = accuracy_score(labels_pred, labels_test)
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+def submitAccuracies():
+  return {"acc":round(acc,3)}
